4172b8419118c11d4d2c332cc9ca7c033d2cf5ab
myfeeling0916@gmail.com <myfeeling0916@gmail.com>
2015-10-21 09:51:12 +0000

    [fixed] http sample code content

--- Node.js/01.Sample/TCP.UDP.HTTP.HTTPS/http/clientHttp.js
+++ Node.js/01.Sample/TCP.UDP.HTTP.HTTPS/http/clientHttp.js
@@ -1,18 +1,30 @@
 var http = require('http');
+/*
+ * {host} - domain name. default localhost.
+ * {hostname} - server name.
+ * {port} - server port. default 80.
+ * {localAddress} - create network connection local network adapter.
+ * {socketPath} - domain socket path.
+ * {method} - HTTP connection method. defalut GET.
+ * {path} - request path. defalut /.
+ * {headers} - request headers
+ * {auth} - Basic Authentication
+**/
 var option = {
-		hostname: '127.0.0.1',
-		port: 1337,
-		path: '/',
-		method: 'GET'
+	hostname: '127.0.0.1',
+	port: 2222,
+	path: '/',
+	method: 'GET'
 };
 
 var req = http.request(option, function(res) {
-		console.log('STATUS: ' + res.statusCode);
-		console.log('HEADRS: ' + JSON.stringify(res.headers));
-		res.setEncoding('utf8');
-		res.on('data', function (chunk) {
-				console.log(chunk);
-		});
+	console.log('STATUS: ' + res.statusCode);
+	console.log('HEADERS: ' + JSON.stringify(res.headers));
+	res.setEncoding('utf8');
+	res.on('data', function(chunk) {
+		console.log(chunk);
+	});
 });
 
+console.log('running server 127.0.0.1:2222');
 req.end();
